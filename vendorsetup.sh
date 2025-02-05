36826f67d03b48e3c2c1e70991d6ce8cd6e5615e
Shorif Uddin <gooogleabal@gmail.com>
2025-02-05 10:10:44 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,61 +1,61 @@
-# TWRP vendorsetup.sh for NX669J
+# TWRP & OrangeFox vendorsetup.sh for NX669J
 
 export ALLOW_MISSING_DEPENDENCIES=true
 
 FDEVICE="NX669J"
 
-twrp_get_target_device() {
+get_target_device() {
 local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
    if [ -n "$chkdev" ]; then 
-      TWRP_BUILD_DEVICE="$FDEVICE"
+      BUILD_DEVICE="$FDEVICE"
    else
       chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
-      [ -n "$chkdev" ] && TWRP_BUILD_DEVICE="$FDEVICE"
+      [ -n "$chkdev" ] && BUILD_DEVICE="$FDEVICE"
    fi
 }
 
-if [ -z "$1" -a -z "$TWRP_BUILD_DEVICE" ]; then
-   twrp_get_target_device
+if [ -z "$1" -a -z "$BUILD_DEVICE" ]; then
+   get_target_device
 fi
     
-if [ "$1" = "$FDEVICE" -o "$TWRP_BUILD_DEVICE" = "$FDEVICE" ]; then
-
+if [ "$1" = "$FDEVICE" -o "$BUILD_DEVICE" = "$FDEVICE" ]; then
     export LC_ALL="C"
 
-# Device Info
+    # TWRP Variables
     export TARGET_DEVICE_ALT="NX669J,NX669S,NX669J-UN,NX669S-UN,NX669J-EEA,NX669S-EEA"
     export TARGET_SUPPORTS_AB=true
-    export TW_DEFAULT_LANGUAGE="en"
     export TW_THEME="portrait_hdpi"
     export TW_USE_TOOLBOX=true
     export TW_INCLUDE_NTFS_3G=true
     export TW_NO_SCREEN_TIMEOUT=true
     export TW_SCREEN_BLANK_ON_BOOT=false
-    export TW_INPUT_BLACKLIST="hbtp_vm"
     export TW_EXCLUDE_TWRPAPP=1
     export TARGET_COPY_OUT_RECOVERY=1
 
-# Encryption & Filesystem
-    export TW_INCLUDE_CRYPTO=1
-    export TW_INCLUDE_FBE=1
-    export TW_USE_FSCRYPT_POLICY=2
-    export TW_INCLUDE_RESETPROP=1
-    export TW_INCLUDE_LIBRESETPROP=1
-    export TW_INCLUDE_FUSE_EXFAT=1
-    export TW_INCLUDE_NTFS_3G=1
-
-# Debug & Tools
-    export TARGET_USES_LOGD=true
-    export TARGET_USES_ASHMEM=true
-    export TW_NO_HAPTICS=false
-    export TARGET_RECOVERY_PIXEL_FORMAT="RGBX_8888"
+    # OrangeFox Variables
+    export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+    export FOX_VERSION="R11.1"
+    export FOX_BUILD_DEVICE="NX669J"
+    export FOX_AB_DEVICE=1
+    export FOX_REPLACE_BUSYBOX_PS=1
+    export FOX_REPLACE_TOOLBOX_GETPROP=1
+    export FOX_DELETE_AROMAFM=1
+    export OF_SCREEN_H=2340
+    export OF_STATUS_H=100
+    export OF_USE_LOCKSCREEN_BUTTON=1
+    export FOX_USE_SPECIFIC_MAGISK_ZIP="~/magisk.zip"
+    export TW_DEVICE_VERSION="R11.1"
 
     # Let's log the build variables
-    if [ -n "$TWRP_BUILD_LOG_FILE" -a -f "$TWRP_BUILD_LOG_FILE" ]; then
-       export | grep "TW_" >> $TWRP_BUILD_LOG_FILE
-       export | grep "TARGET_" >> $TWRP_BUILD_LOG_FILE
+    if [ -n "$BUILD_LOG_FILE" -a -f "$BUILD_LOG_FILE" ]; then
+       export | grep "FOX" >> $BUILD_LOG_FILE
+       export | grep "OF_" >> $BUILD_LOG_FILE
+       export | grep "TARGET_" >> $BUILD_LOG_FILE
+       export | grep "TW_" >> $BUILD_LOG_FILE
     fi
 fi
 
 add_lunch_combo twrp_NX669J-eng
 add_lunch_combo twrp_NX669J-userdebug
+add_lunch_combo orangefox_NX669J-eng
+add_lunch_combo orangefox_NX669J-userdebug
