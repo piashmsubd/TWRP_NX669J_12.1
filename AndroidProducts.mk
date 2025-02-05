9753e3d875258edda20530c49e4223a9c8443654
Shorif Uddin <gooogleabal@gmail.com>
2025-02-05 10:17:45 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,9 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_NX669J.mk
+    $(LOCAL_DIR)/twrp_NX669J.mk \
+    $(LOCAL_DIR)/fox_NX669J.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_NX669J-user \
+    twrp_NX669J-eng \
     twrp_NX669J-userdebug \
-    twrp_NX669J-eng
+    orangefox_NX669J-eng \
+    orangefox_NX669J-userdebug
